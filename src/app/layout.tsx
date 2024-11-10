182fffb4fe82cfe653b4c42ffa384788e9c94d57
Nikhil Tidke <nikhil.tidke@devrev.ai>
2024-11-11 00:28:18 +0530

    fix: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,30 +51,7 @@ export default function RootLayout({
       className={cx("font-sans", newsreader.variable, inter.variable)}
       suppressHydrationWarning
     >
-      <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MXLQNN5B');
-          `}
-        </Script>
-      </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MXLQNN5B"
-            height="0"
-            width="0"
-            style={{
-              display: "none",
-              visibility: "hidden",
-            }}
-          ></iframe>
-        </noscript>
-
         <ThemeProvider defaultTheme="dark" attribute="class">
           <div className="blur" />
           {children}
