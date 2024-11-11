fd06ed2c9f37ed548fad24af111cbcf83eb2e861
Nikhil Tidke <nikhil.tidke@devrev.ai>
2024-11-11 20:33:53 +0530

    fix: new font

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -3,14 +3,11 @@ import { Inter, Newsreader } from "next/font/google";
 export const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
-  display: 'swap',
   variable: "--font-inter",
 });
 
-export const newsreader = Newsreader({
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
-  style: ["normal", "italic"],
-  display: 'swap',
+export const newsreader = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-news",
 });
