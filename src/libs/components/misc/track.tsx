6567c44fde2ffa5b57003261234d32672e078a58
Nikhil Tidke <nikhil.tidke@devrev.ai>
2024-11-11 20:24:23 +0530

    fix: new font

--- src/libs/components/misc/track.tsx
+++ src/libs/components/misc/track.tsx
@@ -4,7 +4,7 @@ import { Link } from "../atoms/link";
 
 export const Track: React.FC<any> = async () => {
   const fetchTracks = async () => {
-    const response = await fetch(process.env.NEXT_PUBLIC_VERCEL_URL + "/api/music");
+    const response = await fetch(process.env.URL ?? "" + "/api/music");
 
     if (response.ok) {
       const data = await response.json();
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -3,12 +3,14 @@ import { Inter, Newsreader } from "next/font/google";
 export const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  display: 'swap',
   variable: "--font-inter",
 });
 
 export const newsreader = Newsreader({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
   style: ["normal", "italic"],
+  display: 'swap',
   subsets: ["latin"],
   variable: "--font-news",
 });
